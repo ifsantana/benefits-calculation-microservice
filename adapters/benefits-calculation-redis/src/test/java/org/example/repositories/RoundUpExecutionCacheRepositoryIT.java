58d1ae03c769525e1b6dd4dd91e581ddf5ac6a88
italo.santana <italo.santana@distilled.ie>
2023-03-28 21:14:16 +0100

    WIP:
    - organizing project structure
    - adding documentation
    - adding integration tests

---
+++ adapters/benefits-calculation-redis/src/test/java/org/example/repositories/RoundUpExecutionCacheRepositoryIT.java
@@ -0,0 +1,48 @@
+package org.example.repositories;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.example.repositories.interfaces.RoundUpExecutionCacheRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+class RoundUpExecutionCacheRepositoryIT {
+  private RoundUpExecutionCacheRepository cacheRepository;
+
+  // container {
+  @Container
+  public GenericContainer redis = new GenericContainer(DockerImageName.parse("redis:5.0.3-alpine"))
+      .withExposedPorts(6379);
+
+  @BeforeEach
+  public void setUp() {
+    cacheRepository = new RoundUpExecutionCacheRepositoryImp();
+    this.cacheRepository.upsertLastProcessedTransactionDayByUserId("123", "2023-03-10T12:48:00.000Z");
+  }
+
+  @Test
+  void shouldReturnCachedRoundUpDate() {
+    var result = this.cacheRepository.getLastProcessedTransactionsDayByUserId("123");
+    assertThat(result).isNotNull();
+    assertThat(result).isEqualTo("2023-03-10T12:48:00.000Z");
+  }
+
+  @Test
+  void shouldNotReturnCachedRoundUpDate() {
+    var result = this.cacheRepository.getLastProcessedTransactionsDayByUserId("1");
+    assertThat(result).isNullOrEmpty();
+  }
+
+  @Test
+  void shouldReturnUpsertRoundUpDate() {
+    this.cacheRepository.upsertLastProcessedTransactionDayByUserId("12345", "2023-03-15T12:48:00.000Z");
+    var result = this.cacheRepository.getLastProcessedTransactionsDayByUserId("12345");
+    assertThat(result).isNotNull();
+    assertThat(result).isEqualTo("2023-03-15T12:48:00.000Z");
+  }
+}
